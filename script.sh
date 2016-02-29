16dfe539d60f259f2f87c82a7b274381a8a34f0a
Ohelig <zorum50@gmail.com>
2016-02-29 17:11:42 -0600

    files

--- script.sh
+++ script.sh
@@ -1,7 +1,8 @@
-sudo apt-get purge open-iscsi, apache2, mysql-server
+sudo apt-get purge open-iscsi, apache2, mysql-server, mysql-client, mysql-common, mysql-client-5.0, mysql-server-5.0
 wget https://github.com/Ohelig/ccdcfiles/raw/master/unauth.cfg --no-check-certificate
 wget https://github.com/Ohelig/ccdcfiles/raw/master/script2.sh --no-check-certificate
 mkdir /etc/update-manager/release-upgrades.d/
 mv unauth.cfg /etc/update-manager/release-upgrades.d/unauth.cfg
-vim /etc/fstab #nobootwait 
+vim /etc/fstab #comment out the problem
 sudo do-release-upgrade
+#have fun :)
