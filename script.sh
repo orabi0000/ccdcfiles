ce91a0d3c7b8e4e78909600b35fbf72570ee1991
Ohelig <zorum50@gmail.com>
2016-02-29 17:14:03 -0600

    files

--- script.sh
+++ script.sh
@@ -1,4 +1,4 @@
-sudo apt-get purge open-iscsi, apache2, mysql-server, mysql-client, mysql-common, mysql-client-5.0, mysql-server-5.0
+sudo apt-get purge open-iscsi apache2 mysql-server mysql-client mysql-common mysql-client-5.0 mysql-server-5.0
 wget https://github.com/Ohelig/ccdcfiles/raw/master/unauth.cfg --no-check-certificate
 wget https://github.com/Ohelig/ccdcfiles/raw/master/script2.sh --no-check-certificate
 mkdir /etc/update-manager/release-upgrades.d/
