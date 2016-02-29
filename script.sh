130c8987bd481f5613cfeea539fd2fb435cb1e40
Ohelig <zorum50@gmail.com>
2016-02-29 14:53:30 -0600

    files

--- script.sh
+++ script.sh
@@ -1,6 +1,6 @@
 sudo apt-get remove open-iscsi
 wget https://github.com/Ohelig/ccdcfiles/raw/master/unauth.cfg --no-check-certificate
+mkdir /etc/update-manager/release-upgrades.d/
 mv unauth.cfg /etc/update-manager/release-upgrades.d/unauth.cfg
-sudo do-release-upgrade
-sudo apt-get install linux-image-3.0.0-32-generic-pae
+vim /etc/fstab #nobootwait 
 sudo do-release-upgrade
