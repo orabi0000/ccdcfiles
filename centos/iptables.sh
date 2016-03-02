f03514c2950e8444ff8eb5ae97b70039b222187f
Jakkin <dracogoyle@gmail.com>
2016-03-02 08:31:20 -0600

    updated iptables

--- centos/iptables.sh
+++ centos/iptables.sh
@@ -1,5 +1,5 @@
-iptables -N TCP
-iptables -N UDP
+#iptables -N TCP
+#iptables -N UDP
 iptables -P FORWARD DROP
 iptables -P INPUT DROP
 iptables -P OUTPUT ACCEPT
@@ -9,6 +9,6 @@ iptables -A INPUT -m conntrack --ctstate INVALID -j DROP
 iptables -A INPUT -p icmp --icmp-type 8 -m conntrack --ctstate NEW -j ACCEPT
 iptables -A INPUT -p udp -m conntrack --ctstate NEW -j UDP
 iptables -A INPUT -p tcp --syn -m conntrack --ctstate NEW -j TCP
-iptables -A TCP -p tcp --dport 80 -j ACCEPT
-iptables -A TCP -p tcp --dport 443 -j ACCEPT
+iptables -A INPUT -p tcp --dport 80 -j ACCEPT
+iptables -A INPUT -p tcp --dport 443 -j ACCEPT
 service iptables save
