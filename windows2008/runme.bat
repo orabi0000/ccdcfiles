fc4366e88d2fa2597c8f9f01d8f3da73c47d1cbf
Dan Salmon <dan.sa7mon@gmail.com>
2016-03-01 22:34:11 -0600

    Add Windows Updates

--- windows2008/runme.bat
+++ windows2008/runme.bat
@@ -1,3 +1,5 @@
+Import-Module ActiveDirectory
+
 dism /online /disable-feature /featurename:Printing-Server-Role
 dism /online /disable-feature /featurename:DHCPServer
 dism /online /disable-feature /featurename:IIS-WebServer
@@ -18,3 +20,9 @@ Remove-ADComputer -Identity ubuntu-ws
 net user IWAM_SHAREPEG /del
 net user IUSR_SHAREPEG /del
 net user HVU_FILESERVER1 /del
+
+wusa.exe AMD64-all-windows6.1-kb2562485-x64_96ab7ea10b66fce4f2b302c76e0fb7d84c44af97.msu
+wusa.exe AMD64-all-windows6.1-kb3042553-x64_5692cc4ea7937ecd6053c76d705c1c3d9f58afdb.msu
+wusa.exe IA64-all-windows6.1-kb2992611-ia64_3ca13daf603bfe0760ee40d8a83f515b9949dc8d.msu
+wusa.exe IA64-all-windows6.1-kb3018238-ia64_086fa14f72e544eb1d6b241595e25b3ad40ff5c1.msu
+
