fd9a36e1304b882e1b19f2ba5a8290a5c8e2a022
Dan Salmon <dan.sa7mon@gmail.com>
2016-03-02 18:22:41 -0600

    add patch MS08-067

--- windows2008/runme.bat
+++ windows2008/runme.bat
@@ -26,3 +26,11 @@ wusa.exe AMD64-all-windows6.1-kb3042553-x64_5692cc4ea7937ecd6053c76d705c1c3d9f58
 wusa.exe IA64-all-windows6.1-kb2992611-ia64_3ca13daf603bfe0760ee40d8a83f515b9949dc8d.msu
 wusa.exe IA64-all-windows6.1-kb3018238-ia64_086fa14f72e544eb1d6b241595e25b3ad40ff5c1.msu
 
+REM Patch MS08-067
+netsh
+rpc
+filter
+add rule layer=um actiontype=block
+add condition field=if_uuid matchtype=equal data=4b324fc8-1670-01d3-1278-5a47bf6ee188
+add filter
+quit
