9e1064aa563d06476102074f413c7a6447826b1f
EkaKay <EkaKay@users.noreply.github.com>
2016-03-02 19:08:31 -0600

    Update debian.sh

--- debian/debian.sh
+++ debian/debian.sh
@@ -3,5 +3,11 @@ cat 'deb http://archive.debian.org/debian-security lenny/updates main' >> /etc/a
 apt-get update
 apt-get install debian-archive-keyring
 apt-get install git-core
+apt-get install perl libnet-ssleay-perl openssl libauthen-pam-perl libpam-runtime libio-pty-perl apt-show-versions python
 
+wget http://prdownloads.sourceforge.net/webadmin/webmin_1.780_all.deb
+dpkg -i webmin_1.780_all.deb
 
+apt-get purge samba*
+
+apt-get update; apt-get upgrade;
