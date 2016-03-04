c461587fd0176ad3f824e060b1db8dea29795424
bkosel <bradley.kosel@mnsu.edu>
2016-03-04 10:38:10 -0600

    Update Configure-Networking.ps1
    added logging rules, created firewall exception for echo

--- Windows-2012/Configure-Networking.ps1
+++ Windows-2012/Configure-Networking.ps1
@@ -1,11 +1,19 @@
 Function Configure-Networking-CCDC {
+	# exceptions to allow browsing, windows update, web hosting, and echo service
 	new-netfirewallrule -displayname HTTP-out -direction outbound -action allow -protocol tcp -localport 80,443
 	new-netfirewallrule -displayname HTTP-in -direction inbound -action allow -protocol tcp -localport 80,443
 	new-netfirewallrule -displayname DNS-out -direction outbound -action allow -protocol tcp -localport 53
+	new-netfirewallrule -displayname echo-in -direction inbound  -action allow -protocol tcp -localport 7
+	new-netfirewallrule -displayname echo-out -direction outbound  -action allow -protocol tcp -localport 7
 	
-	set-netfirewallprofile domain,public,private -enabled true -DefaultInboundAction Block -DefaultOutboundAction Block
-	set-NetConnectionProfile -interfacealias ethernet -NetworkCategory private # can't set profile to domain (enforced by domain membership)
+	# turn the firewall on, enable detailed logging, and ensure it's either on domain or private
+	set-netfirewallprofile -name domain,public,private -enabled true -DefaultInboundAction Block -DefaultOutboundAction Block
+	set-NetConnectionProfile -interfacealias ethernet -NetworkCategory private # can't set category to domain (enforced by the domain membership)
+	set-netfirewallprofile -name domain,private -logallowed true -logblocked true -logmaxsizekilobytes 10240
+	set-netfirewallprofile -name domain -logfilename "c:\firelog\domain.log"
+	set-netfirewallprofile -name private -logfilename "c:\firelog\private.log"
 	
+	# disable unneeded networking options
 	set-net6to4configuration -state disabled
 	set-netteredoconfiguration -type disabled
 	set-netisatapconfiguration -state disabled
