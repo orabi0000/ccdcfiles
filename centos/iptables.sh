1cbff32d1cbc80a1ac285773a64b801b84c7a6a4
Jakkin <dracogoyle@gmail.com>
2016-03-07 12:31:03 -0600

    updated iptables for ccdc in 2017

--- centos/iptables.sh
+++ centos/iptables.sh
@@ -13,10 +13,8 @@ $IPT -t mangle -F
 $IPT -t mangle -X
 modprobe ip_conntrack
 PUB_IF="eth0"
-#unlimited
 $IPT -A INPUT -i lo -j ACCEPT
 $IPT -A OUTPUT -o lo -j ACCEPT
-# DROP all incomming traffic
 $IPT -P INPUT DROP
 $IPT -P OUTPUT DROP
 $IPT -P FORWARD DROP
@@ -27,13 +25,10 @@ $IPT -I INPUT -j $SPAMLIST
 $IPT -I OUTPUT -j $SPAMLIST
 $IPT -I FORWARD -j $SPAMLIST
 fi
-# Block sync
 $IPT -A INPUT -i ${PUB_IF} -p tcp ! --syn -m state --state NEW  -m limit --limit 5/m --limit-burst 7 -j LOG --log-level 4 --log-prefix "Drop Sync"
 $IPT -A INPUT -i ${PUB_IF} -p tcp ! --syn -m state --state NEW -j DROP
-# Block Fragments
 $IPT -A INPUT -i ${PUB_IF} -f  -m limit --limit 5/m --limit-burst 7 -j LOG --log-level 4 --log-prefix "Fragments Packets"
 $IPT -A INPUT -i ${PUB_IF} -f -j DROP
-# Block bad stuff
 $IPT  -A INPUT -i ${PUB_IF} -p tcp --tcp-flags ALL FIN,URG,PSH -j DROP
 $IPT  -A INPUT -i ${PUB_IF} -p tcp --tcp-flags ALL ALL -j DROP
 $IPT  -A INPUT -i ${PUB_IF} -p tcp --tcp-flags ALL NONE -m limit --limit 5/m --limit-burst 7 -j LOG --log-level 4 --log-prefix "NULL Packets"
@@ -44,34 +39,30 @@ $IPT  -A INPUT -i ${PUB_IF} -p tcp --tcp-flags SYN,FIN SYN,FIN -j DROP #XMAS
 $IPT  -A INPUT -i ${PUB_IF} -p tcp --tcp-flags FIN,ACK FIN -m limit --limit 5/m --limit-burst 7 -j LOG --log-level 4 --log-prefix "Fin Packets Scan"
 $IPT  -A INPUT -i ${PUB_IF} -p tcp --tcp-flags FIN,ACK FIN -j DROP # FIN packet scans
 $IPT  -A INPUT -i ${PUB_IF} -p tcp --tcp-flags ALL SYN,RST,ACK,FIN,URG -j DROP
-# Allow full outgoing connection but no incomming stuff
 $IPT -A INPUT -i eth0 -m state --state ESTABLISHED,RELATED -j ACCEPT
 $IPT -A OUTPUT -o eth0 -m state --state NEW,ESTABLISHED,RELATED -j ACCEPT
-# Allow ssh
-#$IPT -A INPUT -p tcp --destination-port 22 -j ACCEPT
-# allow incomming ICMP ping pong stuff
 $IPT -A INPUT -p icmp --icmp-type 8 -m state --state NEW,ESTABLISHED,RELATED -j ACCEPT
 $IPT -A OUTPUT -p icmp --icmp-type 0 -m state --state ESTABLISHED,RELATED -j ACCEPT
-# Allow port 53 tcp/udp (DNS Server)
-#$IPT -A INPUT -p udp --dport 53 -m state --state NEW,ESTABLISHED,RELATED -j ACCEPT
-#$IPT -A OUTPUT -p udp --sport 53 -m state --state ESTABLISHED,RELATED -j ACCEPT
-#$IPT -A INPUT -p tcp --destination-port 53 -m state --state NEW,ESTABLISHED,RELATED  -j ACCEPT
-#$IPT -A OUTPUT -p tcp --sport 53 -m state --state ESTABLISHED,RELATED -j ACCEPT
-# Open port 80
+
+## ADD YOUR RULES BELOW
+
+# Allow ssh only within network
+$IPT -A INPUT -p tcp --dport 22 -s 172.20.0.0/16 -m state --state NEW,ESTABLISHED -j ACCEPT
+$IPT -A OUTPUT -p tcp --sport 22 -d 172.20.0.0/16 -m state --state ESTABLISHED -j ACCEPT
+
+# Allow http/https in/output
 $IPT -A INPUT -p tcp --destination-port 80 -j ACCEPT
+$IPT -A OUTPUT -p tcp --dport 80 -j ACCEPT
 $IPT -A INPUT -p tcp --dport 443 -j ACCEPT
-##### Add your rules below ######
-$IPT -I INPUT -p tcp --dport 80 -m connlimit --connlimit-above 20 --connlimit-mask 40 -j DROP
-#$IPT -t nat -A PREROUTING -p tcp --dport 1:20 -j REDIRECT --to-ports 21
-#$IPT -t nat -A PREROUTING -p tcp --dport 22:79 -j REDIRECT --to-ports 21
-#$IPT -t nat -A PREROUTING -p tcp --dport 81:442 -j REDIRECT --to-ports 21
-#$IPT -t nat -A PREROUTING -p tcp --dport 444:65535 -j REDIRECT --to-ports 21
-##### END your rules ############
-# Do not log smb/windows sharing packets - too much logging
-$IPT -A INPUT -p tcp -i eth0 --dport 137:139 -j REJECT
-$IPT -A INPUT -p udp -i eth0 --dport 137:139 -j REJECT
-# log everything else and drop
+$IPT -A OUTPUT -p tcp --dport 443 -j ACCEPT
+
+# Limit connection limits. Prevent dos attacks.
+$IPT -I INPUT -p tcp --dport 80 -m connlimit --connlimit-above 20 --connlimit-mask 32 -j DROP
+$IPT -I INPUT -p tcp --dport 443 -m connlimit --connlimit-above 20 --connlimit-mask 32 -j DROP
+
+# Log input and logs and drop all others.
 $IPT -A INPUT -j LOG
 $IPT -A FORWARD -j LOG
 $IPT -A INPUT -j DROP
+
 exit 0
