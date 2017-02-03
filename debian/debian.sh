c7d9e5716e344e04ecac8bbd54b0cdddeee5e917
Joshua Jordi <JakkinStewart@users.noreply.github.com>
2017-02-02 22:25:39 -0600

    removing webmin

--- debian/debian.sh
+++ debian/debian.sh
@@ -4,11 +4,11 @@ echo 'deb http://archive.debian.org/debian/ lenny contrib main non-free' > /etc/
 echo 'deb http://archive.debian.org/debian-security lenny/updates main' >> /etc/apt/sources.list
 apt-get update
 apt-get install debian-archive-keyring
-apt-get install git-core
-apt-get install perl libnet-ssleay-perl openssl libauthen-pam-perl libpam-runtime libio-pty-perl apt-show-versions python
+#apt-get install git-core
+#apt-get install perl libnet-ssleay-perl openssl libauthen-pam-perl libpam-runtime libio-pty-perl apt-show-versions python
 
-wget http://prdownloads.sourceforge.net/webadmin/webmin_1.831_all.deb
-dpkg --install webmin_1.831_all.deb
+#wget http://prdownloads.sourceforge.net/webadmin/webmin_1.831_all.deb
+#dpkg --install webmin_1.831_all.deb
 
 apt-get purge samba*
 
