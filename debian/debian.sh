65c689fa6991e2f6dde6e131ccba2c93a3f28121
Joshua Jordi <JakkinStewart@users.noreply.github.com>
2017-02-02 19:02:21 -0600

    fixed myles stupidity

--- debian/debian.sh
+++ debian/debian.sh
@@ -1,12 +1,12 @@
-cat 'deb http://archive.debian.org/debian/ lenny contrib main non-free' > /etc/apt/sources.list
-cat 'deb http://archive.debian.org/debian-security lenny/updates main' >> /etc/apt/sources.list
+echo 'deb http://archive.debian.org/debian/ lenny contrib main non-free' > /etc/apt/sources.list
+echo 'deb http://archive.debian.org/debian-security lenny/updates main' >> /etc/apt/sources.list
 apt-get update
 apt-get install debian-archive-keyring
 apt-get install git-core
 apt-get install perl libnet-ssleay-perl openssl libauthen-pam-perl libpam-runtime libio-pty-perl apt-show-versions python
 
-wget http://prdownloads.sourceforge.net/webadmin/webmin_1.780_all.deb
-dpkg -i webmin_1.780_all.deb
+wget http://prdownloads.sourceforge.net/webadmin/webmin_1.831_all.deb
+dpkg --install webmin_1.831_all.deb
 
 apt-get purge samba*
 
