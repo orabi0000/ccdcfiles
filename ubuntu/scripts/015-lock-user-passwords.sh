7cd535584e21ddb4324a77dc34bf43b079f2ff06
Travis Britz <travisbritz@hotmail.com>
2017-02-04 03:37:29 -0600

    added sudo

--- ubuntu/scripts/015-lock-user-passwords.sh
+++ ubuntu/scripts/015-lock-user-passwords.sh
@@ -1,30 +1,30 @@
-passwd -l daemon
-passwd -l bin
-passwd -l sys
-passwd -l sync
-passwd -l games
-passwd -l man
-passwd -l lp
-passwd -l mail
-passwd -l news
-passwd -l uucp
-passwd -l proxy
-passwd -l www-data
-passwd -l backup
-passwd -l list
-passwd -l irc
-passwd -l gnats
-passwd -l nobody
-passwd -l libuuid
-passwd -l dhcp
-passwd -l syslog
-passwd -l klog
-passwd -l bind
-passwd -l adam
-passwd -l statd
-passwd -l administrator
-passwd -l ntp
-passwd -l messagebus
+sudo passwd -l daemon
+sudo passwd -l bin
+sudo passwd -l sys
+sudo passwd -l sync
+sudo passwd -l games
+sudo passwd -l man
+sudo passwd -l lp
+sudo passwd -l mail
+sudo passwd -l news
+sudo passwd -l uucp
+sudo passwd -l proxy
+sudo passwd -l www-data
+sudo passwd -l backup
+sudo passwd -l list
+sudo passwd -l irc
+sudo passwd -l gnats
+sudo passwd -l nobody
+sudo passwd -l libuuid
+sudo passwd -l dhcp
+sudo passwd -l syslog
+sudo passwd -l klog
+sudo passwd -l bind
+sudo passwd -l adam
+sudo passwd -l statd
+sudo passwd -l administrator
+sudo passwd -l ntp
+sudo passwd -l messagebus
 
 echo Setting new password for $( whoami )...
 passwd
